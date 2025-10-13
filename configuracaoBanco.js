a96333922e7b10bc38b5e132611f23045917292b
eversondeveloper <eversonsilvadeveloper@gmail.com>
2025-10-12 22:01:42 -0300

    Alteração do arquivo de configuração do banco de dados

--- configuracaoBanco.js
+++ configuracaoBanco.js
@@ -1,17 +1,5 @@
-// Arquivo: configuracaoBanco.js
-
-// const credenciais = {
-//     connectionString: 'postgresql://neondb_owner:npg_kEOYe5uchGa3@ep-sweet-frog-adnxqtcg-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
-// };
-
-// module.exports = credenciais;
-
 const credenciais = {
-    user: 'postgres',
-    host: 'localhost',
-    database: 'fastcash01',
-    password: '48344834',
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
 };
 
 module.exports = credenciais;
