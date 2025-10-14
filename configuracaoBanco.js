3b29465d2ffc106ea0c2069317021a8339f00472
eversondeveloper <eversonsilvadeveloper@gmail.com>
2025-10-13 22:24:09 -0300

    config

--- configuracaoBanco.js
+++ configuracaoBanco.js
@@ -1,5 +1,8 @@
 const credenciais = {
-    connectionString: process.env.DATABASE_URL,
+    user: 'postgres',
+    host: 'localhost', 
+    database: 'fastcash01',
+    password: '48344834', 
+    port: 5432,
 };
-
 module.exports = credenciais;
