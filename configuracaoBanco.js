3c71c41c8bcd5fe17b09ffe966b8cf8c90622867
eversondeveloper <eversonsilvadeveloper@gmail.com>
2025-11-22 11:01:35 -0300

    Configuração de conexão

--- configuracaoBanco.js
+++ configuracaoBanco.js
@@ -1,8 +1,21 @@
 const credenciais = {
-    user: 'postgres',
-    host: 'localhost', 
-    database: 'fastcash01',
+    user: '559c5dc48dec4355843231dd6a54bc8a',
+    host: 'postgres.shardatabases.app', 
+    database: 'database',
     password: '48344834', 
     port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
 };
 module.exports = credenciais;
+
+
+// const credenciais = {
+//     user: 'postgres',
+//     host: 'localhost', 
+//     database: 'fastcash01',
+//     password: '48344834', 
+//     port: 5432,
+// };
+// module.exports = credenciais;
